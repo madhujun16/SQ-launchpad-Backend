dbf64566caee2d9a1a6f352be0cda89d2a0845fe
Shivanshu Singh <shivanshusom@gmail.com>
2025-12-28 21:41:11 +0530

    Restore model imports in __init__.py

--- app/launchpad/launchpad_api/db_models/__init__.py
+++ app/launchpad/launchpad_api/db_models/__init__.py
@@ -1,4 +1,3 @@
-# Import new models (optional - only if needed for direct imports)
-# from .scoping_approval import ScopingApproval
-# from .approval_action import ApprovalAction
+from .scoping_approval import ScopingApproval
+from .approval_action import ApprovalAction
 
